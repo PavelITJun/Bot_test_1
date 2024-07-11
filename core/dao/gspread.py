51202c83a423adb74cf1908df95dd6ed37ef8353
Pavel <samoesamokhin@yandex.ru>
2024-07-11 14:56:30 +0300

    Cleaner code1

--- core/dao/gspread.py
+++ core/dao/gspread.py
@@ -4,6 +4,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 async def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name("./service.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("", scope)
     client = gspread.authorize(creds)
     return client
